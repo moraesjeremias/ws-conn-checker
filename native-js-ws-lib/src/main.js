dc819ceeefb707772515cf166cc8ace5f1d0b4b2
Jeremy <jeremias@availproject.org>
2024-09-26 09:54:11 -0300

    main: Adds simple js ws conn check script - native lib

---
+++ native-js-ws-lib/src/main.js
@@ -0,0 +1,23 @@
+import WebSocket from "ws";
+
+const WS_SERVER = process.env.WS_SERVER
+const ws = new WebSocket(WS_SERVER);
+
+// When the connection is open, log a message
+ws.onopen = () => {
+    console.log('Connected to WebSocket server');
+};
+
+ws.on('ping', () => {
+    console.log("Received a ping")
+})
+
+// Handle any errors
+ws.onerror = (error) => {
+    console.error('WebSocket error:', error);
+};
+
+// Log the WebSocket closing event
+ws.onclose = (event) => {
+    console.log('WebSocket connection closed:', event);
+};
